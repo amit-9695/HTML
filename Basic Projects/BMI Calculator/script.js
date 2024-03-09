25d5899ac91a32edfacb3e09133ca904ba806965
AMIT SHUKLA <amit800blp@gmail.com>
2024-03-09 20:05:01 +0530

    Add color Property in Javascript

--- Basic Projects/BMI Calculator/script.js
+++ Basic Projects/BMI Calculator/script.js
@@ -16,13 +16,26 @@ form.addEventListener('submit', function(e){
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
         if (bmi <18.6){
             result.innerHTML = 'Under Weight : '+ bmi;
+            document.getElementById('result').style.color='#ADD8E6';
         }
         else if (bmi >=18.6 && bmi < 24.9){
             result.innerHTML = 'Normal  : '+ bmi;
+            document.getElementById('result').style.color='green';
+        }
+
+        else if(bmi>=25 && bmi< 29.9){
+            result.innerHTML = 'Over weight  : '+ bmi;
+            document.getElementById('result').style.color='yellow';
+        }
+
+        else if (bmi>=30 && bmi<39.9){
+            result.innerHTML = 'Obese  : '+ bmi;
+            document.getElementById('result').style.color='red';
         }
 
         else{
-            result.innerHTML = 'Over weight  : '+ bmi;
+            result.innerHTML = 'Extreme Obese  : '+ bmi;
+            document.getElementById('result').style.color='purple';
         }
     }
 })
