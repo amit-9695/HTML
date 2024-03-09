c531847db428aa99395a5d2bd7cfb12dda2cca22
AMIT SHUKLA <amit800blp@gmail.com>
2024-03-09 19:28:19 +0530

    fix the js code to calculate the BMI

--- Basic Projects/BMI Calculator/script.js
+++ Basic Projects/BMI Calculator/script.js
@@ -7,13 +7,22 @@ form.addEventListener('submit', function(e){
     const result = document.querySelector('#result');
 
     if (height === '' || height < 0 || isNaN(height)){
-        result.innerHTML = 'Please give a value Height ${height}';
+        result.innerHTML = 'Please give a value Height';
     }
     else if (weight === '' || weight < 0 || isNaN(weight)){
-        result.innerHTML = 'Please give a value Weight ${weight}';
+        result.innerHTML = 'Please give a value Weight';
     }
     else{
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
-        result.innerHTML = '<span>${bmi}</span>';
+        if (bmi <18.6){
+            result.innerHTML = 'Under Weight : '+ bmi;
+        }
+        else if (bmi >=18.6 && bmi < 24.9){
+            result.innerHTML = 'Normal  : '+ bmi;
+        }
+
+        else{
+            result.innerHTML = 'Over weight  : '+ bmi;
+        }
     }
 })
